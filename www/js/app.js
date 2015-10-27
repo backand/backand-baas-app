d1a510e5fa18bc1cd23ad4a11328e6323e918bde
kornatzky <kornatzky@gmail.com>
2015-10-27 09:29:55 +0200

    Push Registration for Android

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaDevice, $cordovaPush, $cordovaDialogs, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,6 +18,91 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
+    }
+
+    console.log($cordovaDevice.getPlatform());
+
+    switch($cordovaDevice.getPlatform())
+    {
+      case "Android":
+        var androidConfig = {
+          "senderID": "132184595434",
+        };
+        $cordovaPush.register(androidConfig).then(
+          
+          function(result) {
+            // Success
+            console.log("android push register success", result);
+            $cordovaDialogs.alert(JSON.stringify(result), 'Push Register Success', 'OK')
+            .then(function() {
+              // callback success
+            });
+          }, 
+
+          function(err) {
+            // Error
+            console.log("android push register error", err);
+            $cordovaDialogs.alert(JSON.stringify(err), 'Push Register Failed', 'OK')
+            .then(function() {
+              // callback success
+            });
+          }
+        );
+
+        $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+          switch(notification.event) {
+            case 'registered':
+              if (notification.regid.length > 0 ) {
+                // alert('registration ID = ' + notification.regid);
+                $cordovaDialogs.alert('Registration ID = '  + notification.regid, 'Android Push Id', 'OK')
+                .then(function() {
+                  // callback success
+                });
+              }
+              break;
+
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              // alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+              $cordovaDialogs.alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt, 'Android Push Message', 'OK')
+              .then(function() {
+                  // callback success
+              });
+              break;
+
+            case 'error':
+              // alert('GCM error = ' + notification.msg);
+              $cordovaDialogs.alert('GCM error = ' + notification.msg, 'GCM Error', 'OK')
+              .then(function() {
+                  // callback success
+              });
+              break;
+
+            default:
+              // alert('An unknown GCM event has occurred');
+              $cordovaDialogs.alert('An unknown GCM event has occurred', 'Unknown GCM', 'OK')
+              .then(function() {
+                  // callback success
+              });
+              break;
+          }
+        });
+
+        // WARNING: dangerous to unregister (results in loss of tokenID)
+        // $cordovaPush.unregister(options).then(function(result) {
+        //   // Success!
+        // }, function(err) {
+        //   // Error
+        // });
+
+
+      break;
+
+      case "iOS":
+      break;
+
+      default:
+      break;
     }
   });
 })
